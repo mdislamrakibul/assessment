cdc70890c2cc8fd1e8d67b2750aa81d80d4dea57
rakibul_developer <rakibul.nexkraft@gmail.com>
2022-02-05 21:48:05 +0600

    change api path

--- src/app/services/custom-http.service.ts
+++ src/app/services/custom-http.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class CustomHttpService {
 
-  api = environment.api;
+  api = 'http://api.quotable.io/';
   constructor(
     private _http: HttpClient,
   ) {
